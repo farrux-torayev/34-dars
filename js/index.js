4a4e12b310d1f1e8c175da47cdab2c7df186bc45
farrux-torayev <farruxtorayev590@gmail.com>
2024-11-26 20:19:40 +0500

    fix init

--- js/index.js
+++ js/index.js
@@ -1,24 +1,23 @@
 tailwind.config = {
-    theme: {
-      container: {
-        center: true,
-        width: {
-          DEFAULT: "100%",
-          t: "600px",
-          lt: "960px",
-          d: "1200px",
-        },
-        padding: {
-          DEFAULT: "20px",
-        }
+  theme: {
+    container: {
+      center: true,
+      width: {
+        DEFAULT: "100%",
+        d: "1200px",
+      },
+      padding: {
+        DEFAULT: "15px",
       },
       screens: {
-        t: "640px",
-        lt: "1000px",
-        d: "1240px",
+        d: "1200px",
       },
-      extends: {
-        color: {},
-      }
     },
-  };
+    screens: {
+      d: "1200px",
+    },
+    extends: {
+      color: {},
+    },
+  },
+};
